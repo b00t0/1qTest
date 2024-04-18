a13bacc054aa8429586ce7fa4e5031054f3bf721
Igor Bobkov <bobkovid@gmail.com>
2024-04-18 18:49:02 +0300

    Added LimitCharsOfArray method

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив 
+// из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив 
+// можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+
+Console.Clear();
+
+string[] LimitCharsOfArray(string[] array, int limit)
+{
+    string[] newArray = new string[array.Length];
+    int j = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < limit)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
